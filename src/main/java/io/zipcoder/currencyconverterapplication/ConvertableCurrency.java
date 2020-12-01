7bb9594f930f49003964a786fd71f652c380cfb9
gunjan <pandya.gunjan@gmail.com>
2020-11-30 21:28:13 -0500

    done

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,13 @@
 package io.zipcoder.currencyconverterapplication;
 
+import org.omg.CORBA.Current;
+
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+
+        return currencyType.getRate()/this.getType().getRate();
     }
+
+  CurrencyType getType();
+
 }
